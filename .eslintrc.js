4641ee00de24f0dbceb4c76832854d1ed7e071e2
Daniel Nalborczyk <dnalborczyk@gmail.com>
2021-10-31 11:11:07 -0400

    chore: add eslint recommended type checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     'airbnb-typescript/base',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
